e1117fe8b94b26567190315560b4356b2f346a68
LIECOTI <ib6778794@gmail.com>
2026-07-31 18:48:55 +0300

    Update twrp_tucana.mk

--- twrp_tucana.mk
+++ twrp_tucana.mk
@@ -1,9 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
-
 PRODUCT_NAME := twrp_tucana
 PRODUCT_DEVICE := tucana
 PRODUCT_BRAND := Xiaomi
